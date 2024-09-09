86cce02bf2a08280aa189f273a1796824af2e670
cavadibrahimli1 <cenabibrahimov@gmail.com>
2024-09-09 12:22:24 +0300

    refactor: Update include statements in ros_pospac_bridge.hpp and ros_pospac_bridge.cpp
    - Update include statements in ros_pospac_bridge.hpp and ros_pospac_bridge.cpp to use angle brackets instead of double quotes for system headers.
    - Remove unused parameters in createPoseMessage and createImuMessage functions in ros_pospac_bridge.cpp.

--- src/ros_pospac_bridge.cpp
+++ src/ros_pospac_bridge.cpp
@@ -1,76 +1,4 @@
-#include <rclcpp/rclcpp.hpp>
-#include <sensor_msgs/msg/nav_sat_fix.hpp>
-#include <sensor_msgs/msg/imu.hpp>
-#include <geometry_msgs/msg/pose_with_covariance_stamped.hpp>
-#include <geometry_msgs/msg/pose_array.hpp>
-#include <geometry_msgs/msg/twist_with_covariance_stamped.hpp>
-#include <geometry_msgs/msg/pose_stamped.hpp>
-#include <tf2/LinearMath/Quaternion.h>
-#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
-#include <tf2_ros/transform_listener.h>
-#include <tf2_ros/buffer.h>
-#include <tf2_ros/transform_broadcaster.h>
-#include <GeographicLib/UTMUPS.hpp>
-#include <GeographicLib/MGRS.hpp>
-#include <Eigen/Geometry>
-#include <fstream>
-#include <string>
-#include <sstream>
-#include <chrono>
-#include <thread>
-#include <memory>
-#include <cmath>
-
-class RosPospacBridge : public rclcpp::Node {
-public:
-  RosPospacBridge();
-
-private:
-  void publishGpsData();
-  void publishMapToBaseLinkTransform(const geometry_msgs::msg::Pose& lidar_pose, const rclcpp::Time& timestamp);
-  void publishBaseLinkToGnssTransform(const rclcpp::Time& timestamp);
-  Eigen::Quaterniond getQuaternionFromRPY(double roll, double pitch, double yaw);
-  sensor_msgs::msg::NavSatFix createGpsMessage(double latitude, double longitude, double ellipsoid_height,
-                                               double east_sd, double north_sd, double height_sd, rclcpp::Time timestamp);
-  geometry_msgs::msg::PoseWithCovarianceStamped createPoseMessage(double easting, double northing, double altitude,
-                                                                  double roll, double pitch, double yaw,
-                                                                  double east_sd, double north_sd, double height_sd,
-                                                                  double roll_sd, double pitch_sd, double yaw_sd, rclcpp::Time timestamp, const std::string& mgrs);
-  geometry_msgs::msg::PoseStamped createPoseStampedMessage(const geometry_msgs::msg::PoseWithCovarianceStamped& pose_with_covariance);
-  sensor_msgs::msg::Imu createImuMessage(rclcpp::Time timestamp, double x_angular_rate, double y_angular_rate,
-                                         double z_angular_rate, double x_acceleration, double y_acceleration,
-                                         double z_acceleration, double roll, double pitch, double yaw,
-                                         double roll_sd, double pitch_sd, double heading_sd);
-  void publishTwistMessage(double east_velocity, double north_velocity, double up_velocity,
-                           double x_angular_rate, double y_angular_rate, double z_angular_rate, rclcpp::Time sensor_time);
-
-  rclcpp::Publisher<sensor_msgs::msg::NavSatFix>::SharedPtr gps_pub_;
-  rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_;
-  rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pose_pub_;
-  rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr pose_array_pub_;
-  rclcpp::Publisher<geometry_msgs::msg::TwistWithCovarianceStamped>::SharedPtr twist_pub_;
-  rclcpp::Publisher<geometry_msgs::msg::PoseStamped>::SharedPtr pose_stamped_pub_;
-  rclcpp::Publisher<geometry_msgs::msg::PoseArray>::SharedPtr gnss_ins_pose_array_pub_;
-
-  double origin_easting_;
-  double origin_northing_;
-  double initial_altitude_;
-
-  std::string file_path_;
-
-  std::vector<geometry_msgs::msg::Pose> all_poses_;  // To accumulate all poses
-  std::vector<geometry_msgs::msg::Pose> gnss_ins_poses_;  // To accumulate GNSS_INS poses
-  std::shared_ptr<tf2_ros::Buffer> tf_buffer_;
-  std::shared_ptr<tf2_ros::TransformListener> tf_listener_;
-  std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
-
-  struct Transform {
-    double x, y, z, roll, pitch, yaw;
-  } lidar_to_gnss_transform_;  // Updated to reflect lidar-to-GNSS
-
-  // Function to initialize the base link using the point cloud (lidar) pose
-  geometry_msgs::msg::Pose initializeBaseLinkPose(const geometry_msgs::msg::Pose& lidar_pose);
-};
+#include "ros_pospac_bridge/ros_pospac_bridge.hpp"
 
 RosPospacBridge::RosPospacBridge() : Node("ros_pospac_bridge") {
     // Initialize transform buffer and listener
@@ -343,7 +271,7 @@ geometry_msgs::msg::PoseWithCovarianceStamped RosPospacBridge::createPoseMessage
     double roll, double pitch, double yaw,
     double east_sd, double north_sd, double height_sd,
     double roll_sd, double pitch_sd, double yaw_sd, 
-    rclcpp::Time sensor_time, const std::string& mgrs) {
+    rclcpp::Time sensor_time, const std::string& /* mgrs */) {  // Add comment to indicate unused parameter
 
     geometry_msgs::msg::PoseWithCovarianceStamped pose_msg;
     pose_msg.header.stamp = sensor_time;
@@ -384,7 +312,7 @@ geometry_msgs::msg::PoseStamped RosPospacBridge::createPoseStampedMessage(const 
 sensor_msgs::msg::Imu RosPospacBridge::createImuMessage(rclcpp::Time timestamp, double x_angular_rate, double y_angular_rate,
                                                         double z_angular_rate, double x_acceleration, double y_acceleration,
                                                         double z_acceleration, double roll, double pitch, double yaw,
-                                                        double roll_sd, double pitch_sd, double heading_sd) {
+                                                        double /* roll_sd */, double /* pitch_sd */, double /* heading_sd */) {
     // Create the IMU message
     sensor_msgs::msg::Imu imu_msg;
     imu_msg.header.stamp = timestamp;
